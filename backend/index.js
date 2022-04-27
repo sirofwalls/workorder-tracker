14428542a7b914c2458ae0c379084c641614e326
Peter Walser <mail@ptrwlsr.com>
2022-04-26 19:26:24 -0500

    added production settings and env.example

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const path = require('path')
 const colors = require('colors'); // To make pretty colors in the console. Not needed to proper production
 const dotenv = require('dotenv').config();
 const port = process.env.PORT || 5000;
-const {errorHandler} = require('./backend/middleware/errorMiddleware');
-const connectDB = require('./backend/config/db');
+const {errorHandler} = require('./middleware/errorMiddleware');
+const connectDB = require('./config/db');
 const app = express();
 const url = '/api/v1/'; // All requests will need to start with this. (domain.com/[insert this const here])
 
@@ -12,8 +13,16 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
 // Different routes go here, then sends routes to the controllers for processing
-app.use(url + 'invoice', require('./backend/routes/invoiceRoutes'));
-app.use(url + 'techs', require('./backend/routes/techRoutes'));
+app.use(url + 'invoice', require('./routes/invoiceRoutes'));
+app.use(url + 'techs', require('./routes/techRoutes'));
+
+// Location of frontend after build (For production)
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/build')))
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')))
+} else{
+    app.get('/', (req, res) => res.send('Set  mode to production'))
+}
 
 // Uses the error handler for the "throw new Error messages"
 app.use(errorHandler)
